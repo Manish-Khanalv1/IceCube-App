7e3aa9ebb953c4ea9c9865a11db90e20f529a5a1
Manish-Khanalv1 <u1421460@utah.edu>
2025-10-28 20:46:28 -0600

    new image and new look

--- IceCube/ContentView.swift
+++ IceCube/ContentView.swift
@@ -11,6 +11,28 @@ struct ContentView: View {
     @State private var tapCount=0
     var body: some View {
         VStack {
+            
+            // --- New Logos (Side by side) ---
+            // Horizontal Stack
+            HStack {
+                Image("icecube_logo") // <-- Change this to your logo's name
+                .resizable()         // Makes the image resizeable
+                .scaledToFit()       // Fits it to the available space
+//                .frame(height: 80)   // Sets a fixed height (optional)
+//                .padding()
+                            
+                // 2. Add the second image (icecube logo)
+                Image("uofu_physics") // <-- Change this to your logo's name
+                    .resizable()
+                    .scaledToFit()
+                    .frame(height: 60)
+                
+            }
+            .frame(height:80)
+            .padding(.bottom)
+
+            // ------------------------
+
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
